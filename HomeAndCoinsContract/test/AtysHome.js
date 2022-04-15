587f5b53468a421deccaa091e143ee5aaf6d4606
vidal77390 <leo.vidal@wanadoo.fr>
2022-04-15 12:49:06 +0200

    add new test

--- HomeAndCoinsContract/test/AtysHome.js
+++ HomeAndCoinsContract/test/AtysHome.js
@@ -1,25 +1,45 @@
 const { expect } = require("chai");
 
-let AtysContract;
-let provider;
-let ownerAdress;
+describe("AtysHome contract", function () {
+    let AtysContract;
+    let provider;
+    let ownerAdress;
+    let addresse1;
 
-beforeEach(async function () {
-    const [ownerAdress] = await ethers.getSigners();
+    beforeEach(async function () {
+        [ownerAdress, addresse1] = await ethers.getSigners();
 
-    AtysContract = await ethers.getContractFactory("AtysHome");
+        AtysContract = await ethers.getContractFactory("AtysHome");
 
-    const hardhatAtysHome = await AtysContract.deploy();
+        const hardhatAtysHome = await AtysContract.deploy();
 
-    provider = await hardhatAtysHome.connect(ownerAdress);
-});
+        provider = await hardhatAtysHome.connect(ownerAdress);
+    });
 
-describe("Token contract", function () {
-    it("Deployment should assign the total supply of tokens to the owner", async function () {
+    describe("Annoucements", function () {
+        it("Announcements should be empty on init", async function () {
 
-        const announcements = await provider.functions.getAnnouncements.call();
-        console.log(announcements);
+            const response = await provider.functions.getAnnouncements.call();
+            console.log(response);
+            const announcements = response[0];
+            console.log(announcements);
 
-        expect(announcements[0].title).equal(undefined);
+            expect(announcements.length).equal(0);
+        });
+
+        it("Announcements created should be added", async function () {
+
+            const homeTitle = "TestHouse";
+
+            const createAnnouncement = await provider.functions.createAnnouncement(1000, homeTitle, "20 rue de Test", 100, "This is a Test House");
+
+            const response = await provider.functions.getAnnouncements.call();
+            console.log(response);
+            const announcements = response[0];
+            console.log(announcements);
+
+            expect(announcements[0].title).equal(homeTitle);
+        });
     });
 });
+
